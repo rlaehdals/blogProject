bd30550fba2df8cba3804da4d68b1d67e99f1381
rlaehdals <rkdlem48@gmail.com>
2022-02-22 15:25:28 +0900

    fix changed Method Args

--- src/main/java/com/example/league/controller/UserController.java
+++ src/main/java/com/example/league/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.league.controller;
 
 import com.example.league.aop.MethodLog;
+import com.example.league.domain.Address;
 import com.example.league.dto.LoginDto;
 import com.example.league.argumentresolver.SessionConst;
 import com.example.league.argumentresolver.SessionDto;
@@ -24,14 +25,15 @@ public class UserController {
 
     @MethodLog
     @PostMapping("/signup")
-    public ResponseEntity<String> signup(@RequestBody UserDto userDto){
-        userService.signup(userDto);
+    public ResponseEntity<String> signup(@RequestBody UserDto.SignupDto signupDto){
+        userService.signup(signupDto.getEmail(),signupDto.getPassword(),signupDto.getRole(),
+                signupDto.getName(),new Address(signupDto.getCity(),signupDto.getStreet()));
         return new ResponseEntity<>("ok", HttpStatus.OK);
     }
     @MethodLog
     @PostMapping("/login")
-    public ResponseEntity<Long> login(@RequestBody LoginDto loginDto, HttpServletRequest request){
-        SessionDto sessionDto = userService.login(loginDto);
+    public ResponseEntity<Long> login(@RequestBody UserDto.LoginDto loginDto, HttpServletRequest request){
+        SessionDto sessionDto = userService.login(loginDto.getEmail(),loginDto.getPassword());
         HttpSession session = request.getSession();
         session.setAttribute(SessionConst.LOGIN_MEMBER,sessionDto);
         return new ResponseEntity<>(sessionDto.getId(),HttpStatus.OK);

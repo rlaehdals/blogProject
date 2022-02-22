a6949957bc6a3b9cd0a1a5f603dac95541e5dc38
rlaehdals <rkdlem48@gmail.com>
2022-02-22 15:24:21 +0900

    fix UserDto class Refactoring

--- src/main/java/com/example/league/dto/UserDto.java
+++ src/main/java/com/example/league/dto/UserDto.java
@@ -4,14 +4,26 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
-@Data
-@NoArgsConstructor
 public class UserDto {
-    public String email;
-    public String password;
-    public String name;
-    public String role;
-    public String city;
-    public String street;
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class SignupDto{
+        public String email;
+        public String password;
+        public String name;
+        public String role;
+        public String city;
+        public String street;
+
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class LoginDto{
+        public String email;
+        public String password;
+    }
 }
